735babe55c14c14851f8870ddd49cffacc8286ff
Michal Pluta <michalpl2003@gmail.com>
2026-05-20 12:43:38 +0100

    Refactor auto discovery of hosts and refactor defaults and exceptions

--- modules/machines/nixos/default.nix
+++ modules/machines/nixos/default.nix
@@ -5,8 +5,39 @@
 }:
 let
   entries = builtins.attrNames (builtins.readDir ./.);
-  configs = builtins.filter (dir: builtins.pathExists (./. + "/${dir}/configuration.nix")) entries;
-  homeManagerCfg = userPackages: extraImports: {
+  hostNames = builtins.filter (dir: builtins.pathExists (./. + "/${dir}/configuration.nix")) entries;
+
+  hostDefaults = {
+    system = "x86_64-linux";
+    channel = "";
+  };
+
+  hostMeta = name:
+    hostDefaults // (
+      if builtins.pathExists (./. + "/${name}/meta.nix")
+      then import (./. + "/${name}/meta.nix")
+      else { }
+    );
+
+  commonModules = [
+    ../../homelab
+    ../../misc/email
+    ../../misc/tg-notify
+    ../../misc/mover
+    ../../misc/withings2intervals
+    self.inputs.agenix.nixosModules.default
+    self.inputs.disko.nixosModules.disko
+    self.inputs.disko-zfs.nixosModules.default
+    self.inputs.adios-bot.nixosModules.default
+    self.inputs.autoaspm.nixosModules.default
+    self.inputs.fmatrix.nixosModules.default
+    self.inputs.invoiceplane.nixosModules.default
+    (./. + "/_common/default.nix")
+    ../../users/root
+    ../../users/mikey
+  ];
+
+  homeManagerCfg = {
     home-manager.useGlobalPkgs = false;
     home-manager.extraSpecialArgs = {
       inherit (self) inputs;
@@ -16,62 +47,29 @@ let
       self.inputs.nix-index-database.homeModules.nix-index
       ../../users/mikey/dots.nix
       ../../users/mikey/age.nix
-    ]
-    ++ extraImports;
+    ];
     home-manager.backupFileExtension = "bak";
-    home-manager.useUserPackages = userPackages;
+    home-manager.useUserPackages = false;
   };
 in
 {
-
-  flake.nixosConfigurations =
+  flake.nixosConfigurations = lib.genAttrs hostNames (name:
     let
-      nixpkgsMap = {
-        maya = "-unstable";
-      };
-      systemArchMap = {
-        mona = "aarch64-linux";
-      };
-      myNixosSystem =
-        name: self.inputs."nixpkgs${lib.attrsets.attrByPath [ name ] "" nixpkgsMap}".lib.nixosSystem;
+      meta = hostMeta name;
     in
-    lib.listToAttrs (
-      builtins.map (
-        name:
-        lib.nameValuePair name (
-          (myNixosSystem name) {
-            system = lib.attrsets.attrByPath [ name ] "x86_64-linux" systemArchMap;
-            specialArgs = {
-              inherit (self) inputs;
-              self = {
-                nixosModules = self.nixosModules;
-              };
-            };
-
-            modules = [
-              ../../homelab
-              ../../misc/email
-              ../../misc/tg-notify
-              ../../misc/mover
-              ../../misc/withings2intervals
-              self.inputs.agenix.nixosModules.default
-              self.inputs.disko.nixosModules.disko
-              self.inputs.disko-zfs.nixosModules.default
-              self.inputs.adios-bot.nixosModules.default
-              self.inputs.autoaspm.nixosModules.default
-              self.inputs.fmatrix.nixosModules.default
-              self.inputs.invoiceplane.nixosModules.default
-              self.inputs."home-manager${
-                lib.attrsets.attrByPath [ name ] "" nixpkgsMap
-              }".nixosModules.home-manager
-              (./. + "/_common/default.nix")
-              (./. + "/${name}/configuration.nix")
-              ../../users/root
-              ../../users/mikey
-              (homeManagerCfg false [ ])
-            ];
-          }
-        )
-      ) configs
-    );
+    self.inputs."nixpkgs${meta.channel}".lib.nixosSystem {
+      system = meta.system;
+      specialArgs = {
+        inherit (self) inputs;
+        self = {
+          nixosModules = self.nixosModules;
+        };
+      };
+      modules = commonModules ++ [
+        self.inputs."home-manager${meta.channel}".nixosModules.home-manager
+        (./. + "/${name}/configuration.nix")
+        homeManagerCfg
+      ];
+    }
+  );
 }
