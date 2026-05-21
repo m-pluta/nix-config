1445b3a1889e1f12fc4f02254c195ed2873fec06
Michal Pluta <michalpl2003@gmail.com>
2026-05-21 01:04:18 +0100

    Working cloudflared

--- modules/misc/mpluta.dev/default.nix
+++ modules/misc/mpluta.dev/default.nix
@@ -1,25 +1,45 @@
 {
+  config,
+  inputs,
   ...
 }:
 let
   domain = "mpluta.dev";
+  tunnelId = "7a16d95b-031d-483f-befa-d8fdc081fe5c";
   webRoot = ./site;
 in
 {
-  services.caddy = {
+  age.secrets.cloudflared-tunnel.file = "${inputs.secrets}/network/cloudflared-tunnel.age";
+
+  services.caddy.virtualHosts."${domain}" = {
+    useACMEHost = domain;
+    serverAliases = [ "www.${domain}" ];
+    extraConfig = ''
+      root * ${webRoot}
+      file_server
+    '';
+  };
+  services.cloudflared = {
     enable = true;
-    email = "michalpl2003@gmail.com";
-    virtualHosts."${domain}" = {
-      serverAliases = [ "www.${domain}" ];
-      extraConfig = ''
-        file_server
-        root ${webRoot}
-      '';
+    tunnels.${tunnelId} = {
+      credentialsFile = config.age.secrets.cloudflared-tunnel.path;
+      ingress = {
+        "${domain}" = {
+          service = "https://127.0.0.1:443";
+          originRequest = {
+            noTLSVerify = true;
+            originServerName = domain;
+          };
+        };
+        "*.${domain}" = {
+          service = "https://127.0.0.1:443";
+          originRequest = {
+            noTLSVerify = true;
+            originServerName = domain;
+          };
+        };
+      };
+      default = "http_status:404";
     };
   };
-
-  networking.firewall.allowedTCPPorts = [
-    80
-    443
-  ];
 }
