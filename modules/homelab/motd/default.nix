c0a6fbce73d927783ef22bcf2fbb9f4fbb98c46f
Michal Pluta <michalpl2003@gmail.com>
2026-06-16 17:33:16 +0100

    Refactor motd module and enable on mikelab

--- modules/homelab/motd/default.nix
+++ modules/homelab/motd/default.nix
@@ -5,92 +5,101 @@
   ...
 }:
 let
-  enabledNixosServices = lib.attrsets.mapAttrsToList (name: _value: name) (
-    lib.attrsets.filterAttrs (
-      _name: value: value != "enable" && value ? enable && value.enable
-    ) config.homelab.services
-  );
-  monitoredServices = lib.lists.flatten (
-    lib.lists.forEach enabledNixosServices (
-      x:
-      let
-        svc = config.homelab.services.${x};
-      in
-      if (svc ? monitoredServices) then svc.monitoredServices else [ x ]
-    )
-  );
+  # Collect enabled homelab services and their monitored systemd units
+  enabledServices = lib.filterAttrs (
+    _name: value: value != "enable" && value ? enable && value.enable
+  ) config.homelab.services;
 
-  networkInterface = "";
+  monitoredServices = lib.concatMap (
+    name:
+    let
+      svc = enabledServices.${name};
+    in
+    if svc ? monitoredServices then svc.monitoredServices else [ name ]
+  ) (lib.attrNames enabledServices);
+
   motd = pkgs.writeShellScriptBin "motd" ''
     #! /usr/bin/env bash
-    source /etc/os-release
+
+    # Colors
     RED="\e[31m"
     GREEN="\e[32m"
     YELLOW="\e[33m"
     BOLD="\e[1m"
     ENDCOLOR="\e[0m"
-    LOAD1=`cat /proc/loadavg | awk {'print $1'}`
-    LOAD5=`cat /proc/loadavg | awk {'print $2'}`
-    LOAD15=`cat /proc/loadavg | awk {'print $3'}`
 
-    MEMORY=`free -m | awk 'NR==2{printf "%s/%sMB (%.2f%%)\n", $3,$2,$3*100 / $2 }'`
+    # Helper
+    function print_info() {
+      printf "$BOLD  * %-20s$ENDCOLOR %s\n" "$1" "$2"
+    }
 
-    # time of day
+    # Welcome
     HOUR=$(date +"%H")
-    if [ $HOUR -lt 12  -a $HOUR -ge 0 ]
-    then    TIME="morning"
-    elif [ $HOUR -lt 17 -a $HOUR -ge 12 ]
-    then    TIME="afternoon"
+    if [ $HOUR -lt 12 -a $HOUR -ge 0 ]; then
+      TIME="morning"
+    elif [ $HOUR -lt 17 -a $HOUR -ge 12 ]; then
+      TIME="afternoon"
     else
-        TIME="evening"
+      TIME="evening"
     fi
+    printf "$BOLD Good $TIME! Welcome to $(hostname)!$ENDCOLOR\n"
+    printf "\n"
 
+    # Network
+    ${lib.strings.concatMapStrings (
+      iface:
+      let
+        netdev =
+          if iface == "" then ''NETDEV=$(ip -o route get 8.8.8.8 | cut -f 5 -d " ")'' else "NETDEV=${iface}";
+      in
+      ''
+        ${netdev}
+        print_info "IPv4 $NETDEV" "$(ip -4 addr show $NETDEV | grep -oP '(?<=inet\s)\d+(\.\d+){3}')"
+      ''
+    ) config.homelab.motd.networkInterfaces}
 
-    uptime=`cat /proc/uptime | cut -f1 -d.`
+    # System info
+    source /etc/os-release
+    print_info "Release" "$PRETTY_NAME"
+    print_info "Kernel" "$(uname -rs)"
+    printf "\n"
+
+    # Resource usage
+    LOAD1=$(cat /proc/loadavg | awk {'print $1'})
+    LOAD5=$(cat /proc/loadavg | awk {'print $2'})
+    LOAD15=$(cat /proc/loadavg | awk {'print $3'})
+    MEMORY=$(free -m | awk 'NR==2{printf "%s/%sMB (%.2f%%)\n", $3,$2,$3*100 / $2 }')
+    uptime=$(cat /proc/uptime | cut -f1 -d.)
     upDays=$((uptime/60/60/24))
     upHours=$((uptime/60/60%24))
     upMins=$((uptime/60%60))
     upSecs=$((uptime%60))
+    print_info "CPU usage" "$LOAD1, $LOAD5, $LOAD15 (1, 5, 15 min)"
+    print_info "Memory" "$MEMORY"
+    print_info "System uptime" "$upDays days $upHours hours $upMins minutes $upSecs seconds"
 
-    printf "$BOLD Welcome to $(hostname)!$ENDCOLOR\n"
-    printf "\n"
-    ${lib.strings.concatMapStrings (x: "${x}\n") (
-      lib.lists.forEach config.homelab.motd.networkInterfaces (
-        x:
-        lib.strings.concatMapStrings (x: "${x}\n") ([
-          (
-            if x == "" then
-              ''
-                NETDEV=$(ip -o route get 8.8.8.8 | cut -f 5 -d " ")
-              ''
-            else
-              ''
-                NETDEV=${x}
-              ''
-          )
-          ''
-            printf "$BOLD  * %-20s$ENDCOLOR %s\n" "IPv4 $NETDEV" "$(ip -4 addr show $NETDEV | grep -oP '(?<=inet\s)\d+(\.\d+){3}')"
-          ''
-        ])
-      )
-    )}
-    printf "$BOLD  * %-20s$ENDCOLOR %s\n" "Release" "$PRETTY_NAME"
-    printf "$BOLD  * %-20s$ENDCOLOR %s\n" "Kernel" "$(uname -rs)"
-    printf "\n"
-    printf "$BOLD  * %-20s$ENDCOLOR %s\n" "CPU usage" "$LOAD1, $LOAD5, $LOAD15 (1, 5, 15 min)"
-    printf "$BOLD  * %-20s$ENDCOLOR %s\n" "Memory" "$MEMORY"
-    printf "$BOLD  * %-20s$ENDCOLOR %s\n" "System uptime" "$upDays days $upHours hours $upMins minutes $upSecs seconds"
-
+    # Service status
     printf "\n"
     printf "$BOLD Service status$ENDCOLOR\n"
-
+    PAD=${
+      toString (
+        2
+        + lib.foldl' (
+          acc: s:
+          let
+            len = builtins.stringLength s;
+          in
+          if len > acc then len else acc
+        ) 0 monitoredServices
+      )
+    }
     function get_service_status() {
-      if systemctl is-failed "$1" | grep -q 'failed'; then
-        printf "$RED• $ENDCOLOR%-50s $RED[failed]$ENDCOLOR\n" "$1"
-      elif systemctl is-failed "$1" | grep -xq 'active'; then
-        printf "$GREEN• $ENDCOLOR%-50s $GREEN[active]$ENDCOLOR\n" "$1"
+      if systemctl is-active --quiet "$1"; then
+        printf "$GREEN• $ENDCOLOR%-''${PAD}s $GREEN[active]$ENDCOLOR\n" "$1"
+      elif systemctl is-failed --quiet "$1"; then
+        printf "$RED• $ENDCOLOR%-''${PAD}s $RED[failed]$ENDCOLOR\n" "$1"
       else
-        printf "$YELLOW• $ENDCOLOR%-50s $YELLOW[unknown]$ENDCOLOR\n" "$1"
+        printf "$YELLOW• $ENDCOLOR%-''${PAD}s $YELLOW[unknown]$ENDCOLOR\n" "$1"
       fi
     }
     ${lib.strings.concatStrings (lib.lists.forEach monitoredServices (x: "get_service_status ${x}\n"))}
@@ -104,10 +113,13 @@ in
     networkInterfaces = lib.mkOption {
       description = "Network interfaces to monitor";
       type = lib.types.listOf lib.types.str;
-      default = [ networkInterface ];
+      default = [ "" ];
     };
   };
   config = lib.mkIf config.homelab.motd.enable {
     environment.systemPackages = [ motd ];
+    programs.bash.interactiveShellInit = ''
+      ${motd}/bin/motd
+    '';
   };
 }
